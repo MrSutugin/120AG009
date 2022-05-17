80cfb8d63cc8fe451670b3be6b0fa32072850ba6
Pavel Sutugin <mr.sutugin@gmail.com>
2022-05-17 20:22:57 +0300

    dasdasd

--- Gemfile
+++ Gemfile
@@ -19,6 +19,7 @@ gem 'turbo-rails'
 gem 'trestle'
 gem 'trestle-auth'
 gem 'tzinfo-data', platforms: %i[mingw mswin x64_mingw jruby]
+gem 'sqlite3', '~> 1.4'
 group :development, :test do
   gem 'debug', platforms: %i[mri mingw x64_mingw]
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,9 +11,6 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[7.0].define(version: 2022_04_21_054128) do
-  # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
-
   create_table "administrators", force: :cascade do |t|
     t.string "email"
     t.string "password_digest"
